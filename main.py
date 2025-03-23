f6a09cbe5c81521103aba4ddc948ac3c288fbc0b
danace38 <thedanstrongny42@gmail.com>
2025-03-23 15:35:36 -0400

    Added 60 FPS to main.py

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ from constants import *
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
  
     #Infinite loop for the game loop. This will display GUI filled with black color background.
     while True:
@@ -17,9 +19,11 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        
+        # This limits the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
 
-
-
+        
     
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
