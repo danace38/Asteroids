0ce60f6e1fac348cb49f0f40d55d94f9d1e69499
danace38 <thedanstrongny42@gmail.com>
2025-03-23 16:14:32 -0400

    Added player's spaceship in the middle of the GUI

--- main.py
+++ main.py
@@ -3,16 +3,19 @@
 # throughout this file
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0
  
     #Infinite loop for the game loop. This will display GUI filled with black color background.
     while True:
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
         
         #This will check if the user has closed the window and exit the game loop if they do.
