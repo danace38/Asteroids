3b0946de3f1d797cef3f2828dc46e52c332bb738
danace38 <thedanstrongny42@gmail.com>
2025-03-23 17:13:50 -0400

    Added groups to manage objects

--- main.py
+++ main.py
@@ -9,14 +9,24 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0
+
  
     #Infinite loop for the game loop. This will display GUI filled with black color background.
     while True:
-        player.update(dt)
+        updatable.update(dt)
         screen.fill("black")
-        player.draw(screen)
+
+        for obj in drawable:
+            obj.draw(screen)
+        
         pygame.display.flip()
         
         #This will check if the user has closed the window and exit the game loop if they do.
